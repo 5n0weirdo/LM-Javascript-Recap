cf312374febcefc69de2839ecde7fb02d7dbeed5
5n0weirdo <snoweirdo.protonmail.com>
2023-10-10 18:32:52 +0200

    Example of SwitchCase

---
+++ 03/SwitchCaseExample.js
@@ -0,0 +1,15 @@
+const jobRole = "Front-End Developer";
+
+switch(jobRole) {
+    case "Front-End Developer" :
+      console.log("You are a Front End Developer.");
+      break;
+    case "Back-End Developer" :
+      console.log("You are a Back End Developer.");
+      break;
+    case "Full Stack Developer" :
+      console.log("You are a Full Stack Developer.");
+      break;
+    default:
+        console.log("Please state your job role.")
+}
