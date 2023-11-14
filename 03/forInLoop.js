9a35ab31df25dfec6846c6bb451e1cede129f729
5n0weirdo <snoweirdo@protonmail.com>
2023-11-14 08:00:43 +0200

    Grade exercise

--- 03/forInLoop.js
+++ 03/forInLoop.js
@@ -8,9 +8,3 @@ for (let key in person) {
     console.log(key, person[key])
 }
 
-// Not really done this way
-const fruit = ['banana', 'strawberry', 'apple'];
-
-for (let index in fruit) {
-    console.log(key, colors[index])
-}
